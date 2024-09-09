6f72b9e3b45d582a6e3697a8755b8b3b41d5704d
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-09-09 21:38:48 +0200

    Added Social and Talk links.

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,16 +1,26 @@
+<script lang="ts" setup>
+const { social } = useAppConfig()
+</script>
+
 <template>
   <footer class="bg-white">
     <div class="mx-auto max-w-7xl px-6 py-4 md:flex md:items-center md:justify-between lg:px-8">
       <div class="flex justify-center space-x-4 md:order-2">
-        <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
-          <Icon name="mingcute:linkedin-line" size="20" />
-        </AppButton>
-        <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
-          <Icon name="mingcute:social-x-line" size="20" />
-        </AppButton>
-        <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
-          <Icon name="mingcute:github-line" size="20" />
-        </AppButton>
+        <NuxtLink :to="social.linkedIn" :external="true" target="_blank">
+          <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
+            <Icon name="mingcute:linkedin-line" size="20" />
+          </AppButton>
+        </NuxtLink>
+        <NuxtLink :to="social.twitter" :external="true" target="_blank">
+          <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
+            <Icon name="mingcute:social-x-line" size="20" />
+          </AppButton>
+        </NuxtLink>
+        <NuxtLink :to="social.github" :external="true" target="_blank">
+          <AppButton type="inverse" class="h-9 w-9 grid place-content-center">
+            <Icon name="mingcute:github-line" size="20" />
+          </AppButton>
+        </NuxtLink>
       </div>
       <div class="mt-8 md:order-1 md:mt-0">
         <p class="text-center text-sm leading-5 text-gray-500">
--- components/Landing/Projects.vue
+++ components/Landing/Projects.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 interface Project {
   title: string
-  description: string
+  description?: string
   imageUrl: string
   class: string
 }
@@ -9,25 +9,21 @@ interface Project {
 const projects: Project[] = [
   {
     title: 'Sidestream Labs',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.',
     imageUrl: '/projects/labs.png',
     class: 'bg-green-200/25 border-green-200'
   },
   {
     title: 'PixelTranslate',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.',
     imageUrl: '/projects/pixeltranslate.png',
     class: 'bg-orange-200/25 border-orange-200'
   },
   {
     title: 'Rechnungsradar',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.',
     imageUrl: '/projects/rechnungsradar.png',
     class: 'bg-blue-200/25 border-blue-200'
   },
   {
     title: 'sidebase.io',
-    description: 'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo.',
     imageUrl: '/projects/sidebase.png',
     class: 'bg-pink-200/25 border-pink-200'
   },
@@ -53,13 +49,13 @@ const projects: Project[] = [
       <div
         v-for="(project, index) in projects"
         :key="index"
-        class="group relative min-h-[450px] p-4 rounded-lg overflow-hidden border-2 backdrop-blur-md"
+        class="group relative min-h-[350px] p-4 rounded-lg overflow-hidden border-2 backdrop-blur-md"
         :class="project.class"
       >
         <img
           :src="project.imageUrl"
           alt=""
-          class="absolute h-[350px] w-full top-44 left-10 group-hover:scale-105 transition-all duration-500 z-10"
+          class="absolute h-[350px] w-full top-20 left-10 group-hover:scale-105 transition-all duration-500 z-10"
         >
 
         <div class="relative flex flex-col items-start justify-between z-10">
@@ -67,7 +63,7 @@ const projects: Project[] = [
             <h3 class="text-2xl font-semibold">
               {{ project.title }}
             </h3>
-            <p class="line-clamp-3 text-sm leading-6 text-gray-600">
+            <p v-if="project.description" class="line-clamp-3 text-sm leading-6 text-gray-600">
               {{ project.description }}
             </p>
           </div>
