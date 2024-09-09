2fe13b700a40aee83b08f1589478397d56d182ca
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-09-09 18:22:28 +0200

    chore: add sidebase starter

---
+++ content/index.md
@@ -0,0 +1,3 @@
+# Hello!
+
+Welcome to the page.
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-09-09',
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  typescript: {
+    shim: false
+  },
+})
