58d0ee9a3a21c6c60c1533b8976fa34e558c47e1
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-09-10 01:53:30 +0200

    fix: disable documentDriven mode on pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-09',
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxt/fonts', '@nuxt/icon'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+  ],
   experimental: {
     viewTransition: true
   },
