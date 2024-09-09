1ea3c026f58127312a1e3a9aa2dfa9f2f4e6d0d8
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-09-09 20:46:09 +0200

    Added Talks section

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="py-14">
     <LandingHero class="pb-32" />
     <LandingSkills />
-    <AppDivider class="my-20" />
+    <AppDivider class="my-28" />
     <LandingProjects />
+    <AppDivider class="my-28" />
+    <LandingTalks />
   </div>
 </template>
