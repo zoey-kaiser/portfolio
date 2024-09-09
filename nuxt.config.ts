5a528499e6a8fe10a978c054f7b3fba3dfbeb0dc
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-09-10 01:10:11 +0200

    enable view Transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-09-09',
   modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxt/fonts', '@nuxt/icon'],
+  experimental: {
+    viewTransition: true
+  },
   content: {
     documentDriven: true
   },
