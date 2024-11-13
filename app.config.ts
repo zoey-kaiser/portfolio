9f436691460359f46779d14140d692a1d786b9d1
Zoey <zoeykaiser8@gmail.com>
2024-11-13 21:36:12 +0100

    chore: update bsky link

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   social: {
     github: 'https://github.com/zoey-kaiser',
-    bsky: 'https://bsky.app/profile/zoeykaiser.bsky.social',
+    bsky: 'https://bsky.app/profile/zoeykaiser.com',
     twitter: 'https://x.com/zoey_kaiser',
     linkedIn: 'https://www.linkedin.com/in/zoeykaiser/',
     email: 'zoeykaiser8@gmail.com'
