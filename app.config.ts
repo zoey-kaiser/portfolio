73cef1c1afa643e1cfd30969bb6c3dfee149f0d8
Zoey <zoeykaiser8@gmail.com>
2025-01-24 13:57:14 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,6 @@ export default defineAppConfig({
   social: {
     github: 'https://github.com/zoey-kaiser',
     bsky: 'https://bsky.app/profile/zoeykaiser.com',
-    twitter: 'https://x.com/zoey_kaiser',
     linkedIn: 'https://www.linkedin.com/in/zoeykaiser/',
     email: 'zoeykaiser8@gmail.com'
   }
